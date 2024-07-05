a069214b91770bcb42111a9ebd6f3553d7a4505c
RpM-Vic <victorurielmartinezperez@gmail.com>
2024-07-04 20:10:39 -0600

    now you can log in and log out with google

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,8 +6,9 @@ const  {check} = require('express-validator');
 const {login} = require('../controllers/authControl.js');
 const validarCampos = require('../middlewares/validar_campos.js');
 const validarJWT = require('../middlewares/validar-jwt');
+const {googleVerify} = require('../helpers/googleVerify.js');
 
-//const {validaciones}= require('../middlewares');
+//const {validaciones}= require('../middlewares'); //from index but i don't want to make hell of files
 
 
 
@@ -38,12 +39,22 @@ routerAuth.post('/google',[
     ],async (req,res=response)=>{
         const {id_token}= req.body;
 
+        try{
+            const googleUser = await googleVerify(id_token);
+            console.log(googleUser)
+            res.status(200).json({
+                message:'login de google correcto',
+                googleUser
+            })
+        }
+        catch(err){
+            json.status(401).json({
+                msg:'login de google incorrecto',
+                error:err
+            })
 
-
-        res.status(200).json({
-            message:'login de google correcto',
-            id_token
-        })
+            console.log('error en el login de google',err)
+        }
     }
 )
 
